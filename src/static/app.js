6c959c7505a602bf1bbe42f0c48164e5e63c92ba
Ragupathi925 <ragupathi.balakrishnan@atos.net>
2026-03-03 09:20:12 +0000

    Update comment to clarify participants section creation in activities list

--- src/static/app.js
+++ src/static/app.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const spotsLeft = details.max_participants - details.participants.length;
 
-        // Create participants list HTML
+        // Create participants section
         let participantsHTML = "";
         if (details.participants && details.participants.length > 0) {
           participantsHTML = `
